2d86d54d160dc295e26baf0887019e188d8381dd
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-19 22:14:31 +0300

    Fix

--- Day-2-DragNdrop/app.js
+++ Day-2-DragNdrop/app.js
@@ -1,30 +1,30 @@
 const item = document.querySelector('.item'),
 	placeholders = document.querySelectorAll('.placeholder'),
 	dragStart = (event) => {
-		console.log('drag start', event.target)
-		console.log('drag start', this)
+		//console.log('drag start', event.target)
+		//console.log('drag start', this)
 		event.target.classList.add('hold')
 		setTimeout(() => event.target.classList.add('hide'), 0)
 	},
 	dragEnd = (event) => {
-		console.log('drag end')
-		event.target.classList.remove('hold', 'hide')
+		//console.log('drag end')
+		//event.target.classList.remove('hold', 'hide')
 		event.target.className = 'item'
 	},
 	dragOver = (event) => {
-		console.log('drag over')
+		//console.log('drag over')
 		event.preventDefault()
 	},
 	dragEnter = (event) => {
 		event.target.classList.add('hovered')
-		console.log('drag enter')
+		//console.log('drag enter')
 	},
 	dragLeave = (event) => {
 		event.target.classList.remove('hovered')
-		console.log('drag leave')
+		//console.log('drag leave')
 	},
 	dragDrop = (event) => {
-		console.log('drag drop')
+		//console.log('drag drop')
 		event.target.classList.remove('hovered')
 		event.target.append(item)
 	}
@@ -33,7 +33,7 @@ item.addEventListener('dragstart', dragStart)
 item.addEventListener('dragend', dragEnd)
 
 placeholders.forEach((placeholder) => {
-	console.log(placeholder)
+	//console.log(placeholder)
 	placeholder.addEventListener('dragover', dragOver)
 	placeholder.addEventListener('dragenter', dragEnter)
 	placeholder.addEventListener('dragleave', dragLeave)
