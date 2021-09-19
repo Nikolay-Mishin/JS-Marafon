a6b04587c119962af1207137a909d2f03e6167d3
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-19 16:39:24 +0300

    Add slidesPlugin()

--- 1day-cards/app.js
+++ 1day-cards/app.js
@@ -1,17 +1,57 @@
-const slides = document.querySelectorAll('.slide') 
+function slidesPlugin(activeSlide = 0) {
+	const container = document.getElementById('container'),
+		slides = document.querySelectorAll('.slide'),
+		clearActiveClasses = () => [].filter.call(slides, slide => slide.classList.contains('active'))
+			.forEach(slide => slide.classList.remove('active')
+			),
+		data = [
+			{
+				url: '1628107073262-e086371689a6',
+				w: 800
+			},
+			{
+				url: '1628106881749-88bf2fb79103',
+				w: 800
+			},
+			{
+				url: '1583435292794-4803a56c5043',
+				w: 800
+			},
+			{
+				url: '1589360395642-bfb140284700',
+				w: 800
+			},
+			{
+				url: '1628523300960-01e94dec7519',
+				w: 335
+			},
+			{
+				url: '1631295156305-6afe3f42b767',
+				w: 357
+			},
+			{
+				url: '1630090220340-367fba0abb53',
+				w: 282
+			},
+			{
+				url: '1630085808419-1a7029476a2f',
+				w: 334
+			}
+		]
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-        console.log()
-        slide.getElementsByClassName('caption')[0].classList.add('active')
-    })
+	slides[activeSlide].classList.add('active')
+
+	slides.forEach((slide, i) => {
+		slide.setAttribute('style', `background-image: url('https://images.unsplash.com/photo-${data[i].url}?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${data[i].w}&q=80');`)
+		slide.addEventListener('click', () => {
+			//container.querySelector('.active').classList.remove('active')
+			clearActiveClasses()
+			slide.classList.add('active')
+			console.log(container)
+			console.log(slides)
+			console.log(slide.classList)
+		})
+	})
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-        slide.getElementsByClassName('caption')[0].classList.remove('active')
-    })
-}
+slidesPlugin(3)
