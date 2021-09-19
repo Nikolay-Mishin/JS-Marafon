b0f4cc3c6f19d380be7f5eb27eef9f155670fc91
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-19 16:44:36 +0300

    Fix data

--- 1day-cards/app.js
+++ 1day-cards/app.js
@@ -3,23 +3,20 @@ function slidesPlugin(activeSlide = 0) {
 		slides = document.querySelectorAll('.slide'),
 		clearActiveClasses = () => [].filter.call(slides, slide => slide.classList.contains('active'))
 			.forEach(slide => slide.classList.remove('active')
-			),
+		),
+		w = 800,
 		data = [
 			{
-				url: '1628107073262-e086371689a6',
-				w: 800
+				url: '1628107073262-e086371689a6'
 			},
 			{
-				url: '1628106881749-88bf2fb79103',
-				w: 800
+				url: '1628106881749-88bf2fb79103'
 			},
 			{
-				url: '1583435292794-4803a56c5043',
-				w: 800
+				url: '1583435292794-4803a56c5043'
 			},
 			{
-				url: '1589360395642-bfb140284700',
-				w: 800
+				url: '1589360395642-bfb140284700'
 			},
 			{
 				url: '1628523300960-01e94dec7519',
@@ -42,7 +39,7 @@ function slidesPlugin(activeSlide = 0) {
 	slides[activeSlide].classList.add('active')
 
 	slides.forEach((slide, i) => {
-		slide.setAttribute('style', `background-image: url('https://images.unsplash.com/photo-${data[i].url}?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${data[i].w}&q=80');`)
+		slide.setAttribute('style', `background-image: url('https://images.unsplash.com/photo-${data[i].url}?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${data[i].w || w}&q=80');`)
 		slide.addEventListener('click', () => {
 			//container.querySelector('.active').classList.remove('active')
 			clearActiveClasses()
