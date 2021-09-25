94904ef78479e2a8ac7aa2cb748c3857682750d3
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-25 16:13:05 +0300

    Fix register()

--- helpers.js
+++ helpers.js
@@ -1,44 +1,78 @@
 const { log } = console,
 	error = msg => { throw new Error(msg) },
-	getStyles = el => el.currentStyle || getComputedStyle(el, ''), // IE || другой браузер
-	get = (el, target = document) => target.querySelector(el),
-	getAll = (el, target = document) => target.querySelectorAll(el),
-	getHtmlAll = () => getAll('html'),
-	getHtml = () => htmlAll[0],
-	addEvent = (el, event, cb) => (el ? el : document).addEventListener(event, cb),
-	setHtml = (target = '', pos = 'beforeend', html = '') => (target ? target : document).insertAdjacentHTML(pos, html),
-	create = (el = 'div') => document.createElement(el),
-	getRect = (el = document) => el.getBoundingClientRect(),
-	filter = (obj, cb) => [].filter.call(obj, cb),
-	clearClasses = function (target, ...classList) {
-		target.filter(placeholder => {
-			let contains = false;
-			classList.forEach(_class => { if (placeholder.classList.contains(_class)) contains = true });
-			return contains;
-		}).forEach(placeholder => placeholder.classList.remove(classList))
+	hasOwn = (obj, prop) => obj.hasOwnProperty(prop),
+	register = (obj, { value, prop, _, def = false, enumerable = true, configurable = false, writable = false, get, set } = {}) => {
+		//const func = {
+		//	[value.name]: function (...args) { log(this); log(args); log([value.name]); log([value.name]._); /*return [value.name]._(this, ...args)*/ }
+		//}
+		//func[value.name]._ = _
+		//log(func[value.name].name)
+		//log(func[value.name])
+		//log(func[value.name]._)
+		//func[value.name]()
+		prop = prop || value.name
+		if (_) value._ = _
+		if (obj.__proto__ && !hasOwn(obj.__proto__, prop)) {
+			!def ? obj.__proto__[prop] = value :
+				define(obj.__proto__, prop, value, { enumerable, configurable, writable, get, set })
+		}
+		return _ ? _ : value;
 	},
-	register = (obj, prop, value) => obj.__proto__[prop] = value,
-	define = (obj, prop = '', value = null, { enumerable = true, configurable = false, writable = false, get, set } = {}) => {
-		const descDefault = { enumerable, configurable },
-			desc = assign(descDefault, value ? { value, writable } : { get, set });
-		return hasOwn(obj, prop) ? value : Object.defineProperty(obj, prop, desc);
+	define = (obj, { value = null, prop = '', enumerable = true, configurable = false, writable = false, get, set } = {}) => {
+		prop = prop || value.name;
+		return hasOwn(obj, prop) ? value :
+			Object.defineProperty(obj, prop, assign({ enumerable, configurable }, get || set ? { get, set } : { value, writable }));
 	},
-	getProto = (obj, i = 0) => protoList(obj, i),
-	protoList = (function _protoList(obj) {
+	getProto = (obj, i = 0) => protoList(obj)[i],
+	protoList = (function protoList(obj) {
 		const proto = obj ? obj.__proto__ : null;
 		this.objProto = this.objProto || proto;
 		this.protoList = this.protoList || [];
 		if (proto) {
 			this.protoList.push(proto);
-			_protoList.call(this, proto);
+			protoList.call(this, proto);
 		}
 		if (proto == this.objProto) {
-			const protoList = this.protoList;
+			const _protoList = this.protoList;
 			this.objProto = null;
 			this.protoList = [];
-			return protoList;
+			return _protoList;
 		}
-	}).bind({})
+	}).bind({}),
+	getAll = register(getProto(document.querySelectorAll('html')[0]), {
+		value: function getAll(el = 'html') { log(this); return getAll._(el, this) },
+		_: (el = 'html', target = document) => target.querySelectorAll(el)
+	}),
+	nodeList = getAll(),
+	html = nodeList[0],
+	htmlEl = getProto(html),
+	getStyles = el => el.currentStyle || getComputedStyle(el, ''), // IE || другой браузер
+	get = (el = 'html', target = document) => target.querySelector(el),
+	addEvent = (el, event, cb) => (el ? el : document).addEventListener(event, cb),
+	setHtml = (target = '', pos = 'beforeend', html = '') => (target ? target : document).insertAdjacentHTML(pos, html),
+	create = (el = 'div') => document.createElement(el),
+	getRect = (el = document) => el.getBoundingClientRect(),
+	filter = register(nodeList, {
+		value: function filter(cb) { log(this); return filter._(this, cb) },
+		_: (obj, cb) => [].filter.call(obj, cb)
+	}),
+	clearClasses = register(nodeList, {
+		value: function clearClasses(...classList) { log(this); return clearClasses._(this, ...classList) },
+		_: (target, ...classList) => {
+			target.filter(placeholder => {
+				let contains = false;
+				classList.forEach(_class => { if (placeholder.classList.contains(_class)) contains = true });
+				return contains;
+			}).forEach(placeholder => placeholder.classList.remove(classList))
+		}
+	})
 
-register(getHtmlAll(), 'filter', function (cb) { return filter(this, cb) })
-register(getHtmlAll(), 'clearClasses', function (...classList) { return clearClasses(this, ...classList) })
+html.classList.add('active')
+log(html.classList)
+
+log(nodeList.filter(function (el) { return el }))
+
+nodeList.clearClasses('active')
+log(html.classList)
+
+log(html.getAll('body'))
