67da025a56c71235a71ab731ee492af6a2275d29
Николай Мишин <mishin.nikolay.d3@mail.ru>
2021-09-26 20:58:46 +0300

    Fix register

--- helpers.js
+++ helpers.js
@@ -2,82 +2,86 @@ const { log } = console,
 	error = msg => { throw new Error(msg) },
 	{ assign, keys } = Object,
 	{ from } = Array,
-	objProto = {}.__proto__,
+	nullProto = {}.__proto__,
 	hasOwn = (() => {
-		if (!objProto.hasOwnProperty('hasOwn')) {
-			Object.defineProperty(objProto, 'hasOwn', { value: function hasOwn(prop) { return this.hasOwnProperty(prop) } })
+		if (!nullProto.hasOwnProperty('hasOwn')) {
+			Object.defineProperty(nullProto, 'hasOwn', { value: function hasOwn(prop) { return this.hasOwnProperty(prop) } })
 		}
 		return (obj, prop) => obj.hasOwn(prop)
 	})(),
 	define = (() => {
-		function define(obj, value = null, { prop = '', enumerable = false, configurable = false, writable = false, get, set } = {}) {
-			prop = prop || value.name
-			if (!obj.hasOwn(prop)) {
-				Object.defineProperty(obj, prop, assign({ enumerable, configurable }, get || set ? { get, set } : { value, writable }))
-			}
-			return value
-		}
-		if (!objProto.hasOwn('_define')) Object.defineProperty(objProto, '_define', {
+		if (!nullProto.hasOwn('_define')) Object.defineProperty(nullProto, '_define', {
 			value:
 				function _define(value = null, { prop = '', enumerable = false, configurable = false, writable = false, get, set } = {}) { return define(this, ...arguments) }
 		})
-		return define
+		return function define(obj, value = null, { prop = '', enumerable = false, configurable = false, writable = false, get, set } = {}) {
+			prop = prop || value.name
+			if (!obj.hasOwn(prop)) {
+				const desc = assign({ enumerable, configurable }, get || set ? { get, set } : { value, writable })
+				Object.defineProperty(obj, prop, desc)
+			}
+			return value
+		}
 	})(),
 	register = (() => {
-		function register(obj, value, { prop, func, def, enumerable = false, configurable = false, writable = false, get, set } = {}) {
+		nullProto._define(
+			function _register({ prop, value, func, def, enumerable = false, configurable = false, writable = false, get, set } = {}) { return register(this, ...arguments) })
+		return function register(obj, value, { prop, func, def, enumerable = false, configurable = false, writable = false, get, set } = {}) {
 			obj = obj.__proto__
 			value = value[keys(value).shift()] || value
 			prop = prop || value.name
 			if (func) value.func = func
 			else {
 				const _func = {
-					[prop]: function (...args) { log(this); return _func[prop].func(this, ...args) }
+					[prop]: function (...args) { return _func[prop].func(this, ...args) }
 				}
 				_func[prop].func = value
 				func = value
 				value = _func[prop]
 			}
 			if (obj && !obj.hasOwn(prop)) {
-				def || obj === objProto ? obj._define(value, { prop, enumerable, configurable, writable, get, set }) :
+				def || obj === nullProto ? obj._define(value, { prop, enumerable, configurable, writable, get, set }) :
 					obj[prop] = value
 			}
 			return func
 		}
-		if (!objProto.hasOwn('register')) objProto._define(
-			function _register({ prop, value, func, def, enumerable = false, configurable = false, writable = false, get, set } = {}) { return register(this, ...arguments) })
-		return register
 	})(),
-	getProto = (obj, i = 0) => protoList(obj)[i],
-	protoList = (function protoList(obj) {
+	getProto = ({})._register({ getProto(obj = Object, i = 0) { return protoList(obj)[i] } }),
+	protoList = ({})._register(function protoList(obj = Object) {
+		if (this instanceof Window) return protoList.call({}, obj)
 		const proto = obj ? obj.__proto__ : null
 		this.objProto = this.objProto || proto
-		this.protoList = this.protoList || []
+		this._protoList = this._protoList || []
 		if (proto) {
-			this.protoList.push(proto)
+			this._protoList.push(proto)
 			protoList.call(this, proto)
 		}
+		getProto
 		if (proto == this.objProto) {
-			const _protoList = this.protoList
+			const _protoList = this._protoList
 			this.objProto = null
-			this.protoList = []
+			this._protoList = []
 			return _protoList
 		}
-	}).bind({}),
+	}),
 	nodeList = document.querySelectorAll('html'),
 	html = nodeList[0],
-	htmlEl = getProto(html),
-	reverse = obj => from(obj).reverse(),
+	htmlEl = html.getProto(),
+	reverse = ({})._register({ reverse(obj) { return from(obj).reverse() } }),
 	getAll = htmlEl._register(function getAll(el = 'html', target = document) {
-		if (el instanceof HTMLElement) [el, target] = reverse(arguments)
+		if (el instanceof HTMLElement) [el, target] = arguments.reverse()
 		return target.querySelectorAll(el)
 	}),
-	getStyles = el => el.currentStyle || getComputedStyle(el, ''), // IE || другой браузер
-	get = (el = 'html', target = document) => target.querySelector(el),
-	addEvent = (el, event, cb) => (el ? el : document).addEventListener(event, cb),
-	setHtml = (target = '', pos = 'beforeend', html = '') => (target ? target : document).insertAdjacentHTML(pos, html),
+	getStyles = htmlEl._register({ getStyles(el) { return el.currentStyle || getComputedStyle(el, '') } }), // IE || другой браузер
+	get = htmlEl._register({ get(el = 'html', target = document) { return target.querySelector(el) } }),
+	addEvent = htmlEl._register({ addEvent(el, event, cb) { return (el ? el : document).addEventListener(event, cb) } }),
+	setHtml = htmlEl._register({ setHtml(target = '', pos = 'beforeend', html = '') {
+		return (target ? target : document).insertAdjacentHTML(pos, html) } }),
+	//setHtml = (target = '', pos = 'beforeend', html = '') => (target ? target : document).insertAdjacentHTML(pos, html),
 	create = (el = 'div') => document.createElement(el),
-	getRect = (el = document) => el.getBoundingClientRect(),
-	filter = nodeList._register(function filter(obj, cb) { return [].filter.call(obj, cb) }),
+	getRect = htmlEl._register({ getRect(el = document) { return el.getBoundingClientRect() } }),
+	//getRect = (el = document) => el.getBoundingClientRect(),
+	filter = nodeList._register({ filter(obj, cb) { return [].filter.call(obj, cb) } }),
 	clearClasses = nodeList._register(function clearClasses(target, ...classList) {
 		target.filter(placeholder => {
 			let contains = false
@@ -89,16 +93,16 @@ const { log } = console,
 html.classList.add('active')
 log(html.classList)
 
-log(nodeList.filter(function (el) { return el }))
+log(nodeList.filter(el => el))
 
 nodeList.clearClasses('active')
 log(html.classList)
 
 log(html.getAll('body'))
 
-log(objProto)
+log(nullProto)
 
-log(getProto(nodeList))
-log(getProto(htmlEl))
+log(nodeList.getProto())
+log(htmlEl.getProto())
 
-log(getProto(nodeList).hasOwn('filter'))
+log(nodeList.getProto().hasOwn('filter'))
